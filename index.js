b33910a22db08ec632aeeee42af59bd6e7c7df1a
Jimmy Liow <18777396951@163.com>
2023-08-29 15:36:58 +0800

    add exec function

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const github = require('@actions/github');
 const child_process = require('child_process');
 const util = require('util');
 
-try {
+async function main() {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  
-   //define git log script for easy debuggin, this will return data as json file
-   const format = ' --pretty=format:\'{"commit": "%h","author": "%aN","date": "%ad","message": "%f", "original_message": "%s"},\'';
-   const endPart =  "$@ | perl -pe 'BEGIN{print \"[\"}; END{print \"]\"}' | perl -pe 's/},]/}]/'";
-   
-   //get latest tag
-   const latestRelease = await exec('git describe --tags --abbrev=0'); 
-   const logScript = "git log " + latestRelease + "..HEAD " + format + endPart;
-   const logs = await exec(logScript)
-   console.log(`log data: ${logs}!`);
-} catch (error) {
-  core.setFailed(error.message);
+
+  //define git log script for easy debuggin, this will return data as json file
+  const format = ' --pretty=format:\'{"commit": "%h","author": "%aN","date": "%ad","message": "%f", "original_message": "%s"},\'';
+  const endPart = "$@ | perl -pe 'BEGIN{print \"[\"}; END{print \"]\"}' | perl -pe 's/},]/}]/'";
+
+  //get latest tag
+  const latestRelease = await exec('git describe --tags --abbrev=0');
+  const logScript = "git log " + latestRelease + "..HEAD " + format + endPart;
+  const logs = await exec(logScript)
+  console.log(`log data: ${logs}!`);
 }
+
+main();
